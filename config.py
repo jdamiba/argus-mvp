e10944ba65d183f30e2e243ab07b3b4ae9b99670
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-01 17:07:14 -0500

    postgres?

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ rand_string = "".join(
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or rand_string
-    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    SQLALCHEMY_DATABASE_URI = "postgres://grlmzqfyszpebe:ef5a0d0f4e94dfd29cc0aa10141705313de934a6baf16341db6c3509a3837b64@ec2-54-92-174-171.compute-1.amazonaws.com:5432/d6ing6qh2i3ncc"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
