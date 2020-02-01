32a14463c2c412bd59b80764ef06f066b23594bb
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-01 04:01:37 -0500

    switch to cleardb

--- config.py
+++ config.py
@@ -13,10 +13,4 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or rand_string
     SQLALCHEMY_DATABASE_URI = "mysql://b7732efb8cd816:f8ce8f73@us-cdbr-iron-east-04.cleardb.net/heroku_fc83e429e10d4a9"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["jdamiba@gmail.com"]
     POSTS_PER_PAGE = 3
