a1e7a99d29a1d29ac584845ba94e2b9161a8a755
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-01 03:33:27 -0500

    fixup

--- flask_server/forms.py
+++ flask_server/forms.py
@@ -13,7 +13,7 @@ class LoginForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    about_me = TextAreaField("About Me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
 
     def __init__(self, original_username, *args, **kwargs):
