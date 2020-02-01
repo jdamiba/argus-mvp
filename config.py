ec288eddf0be8585b19668bf597679d81d528e21
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-01 02:53:31 -0500

    add followers model and pagination

--- config.py
+++ config.py
@@ -4,9 +4,19 @@ import string
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-rand_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+rand_string = "".join(
+    random.choice(string.ascii_uppercase + string.digits) for _ in range(10)
+)
+
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or rand_string 
+    SECRET_KEY = os.environ.get("SECRET_KEY") or rand_string
     SQLALCHEMY_DATABASE_URI = "mysql://bc401521a801de:76a77075@us-cdbr-iron-east-05.cleardb.net/heroku_17955da1986c614"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["jdamiba@gmail.com"]
+    POSTS_PER_PAGE = 3
--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 from config import Config
 from scouting_report import create_dash_app
 
@@ -15,7 +19,48 @@ login.login_view = "login"
 dash_app = create_dash_app(flask_server)
 
 
-from flask_server import routes, models
+from flask_server import routes, models, errors
 
 if __name__ == "__main__":
     dash_app.run_server(debug=True)
+
+if not flask_server.debug:
+    if flask_server.config["MAIL_SERVER"]:
+        auth = None
+        if flask_server.config["MAIL_USERNAME"] or flask_server.config["MAIL_PASSWORD"]:
+            auth = (
+                flask_server.config["MAIL_USERNAME"],
+                flask_server.config["MAIL_PASSWORD"],
+            )
+        secure = None
+        if flask_server.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(
+                flask_server.config["MAIL_SERVER"],
+                flask_server.config["MAIL_PORT"],
+            ),
+            fromaddr="no-reply@" + flask_server.config["MAIL_SERVER"],
+            toaddrs=flask_server.config["ADMINS"],
+            subject="Microblog Failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        flask_server.logger.addHandler(mail_handler)
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler(
+        "logs/microblog.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    flask_server.logger.addHandler(file_handler)
+
+    flask_server.logger.setLevel(logging.INFO)
+    flask_server.logger.info("Microblog startup")
